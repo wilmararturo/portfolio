57f943ed90114ead93d97e058a86936e480e99e4
Wilmar A. Stephens <wilmars@alumni.brown.edu>
2020-11-23 02:15:43 -0500

    initial commit

---
+++ assets/js/portfolio.js
@@ -0,0 +1,7 @@
+const projectDivEl = document.getElementById("project-row");
+
+const renderProjects = (projectsObj) => {
+    for (const project in projectsObj) {
+        console.log(project[title]);
+    }
+}
